d92326bcc1a197643fb2b39db28966912804d169
aaronkee3 <keeaaronyy@gmail.com>
2023-02-26 13:40:54 +0800

    Changed directory, added flask app

---
+++ webpage/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, flash, redirect, render_template, request, session
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/solveSudoku")
+def solveSudoku():
+    return render_template("solveSudoku.html")
+
+@app.route("/completed")
+def completed():
+    return render_template("completed.html")
